383b7c64e4c184b9f8de7eda7f68b3e52b2699bf
spl <soos.peter.levente@protonmail.com>
2018-10-23 21:15:36 +0200

    licence, getCurrentURL simple callback and a boolean check bugfix

--- src/popup/js/http-scratchmarks-popup.js
+++ src/popup/js/http-scratchmarks-popup.js
@@ -1,3 +1,24 @@
+/* 2018 Soos Peter Levente. Licensed under the MIT license.
+
+Permission is hereby granted, free of charge, to any person obtaining
+a copy of this software and associated documentation files (the
+"Software"), to deal in the Software without restriction, including
+without limitation the rights to use, copy, modify, merge, publish,
+distribute, sublicense, and/or sell copies of the Software, and to
+permit persons to whom the Software is furnished to do so, subject to
+the following conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE. */
+
 (function () {
 
   "use strict"; const // functional yay!
@@ -296,7 +317,7 @@
 
   addDomainRule = event => {
     editTitle.text("Add new rule");
-    getCurrentURL().then(url => pathInput.val(url.host));
+    getCurrentURL(url => pathInput.val(url.host));
     showEdit();
     searchInput.focus();
   },
@@ -372,7 +393,7 @@
   // None of them yonder Rule objects.
   ruleObject = (type, search, replace, enabled) => {
     return {
-      ruleIsEnabled: enabled || true,
+      ruleIsEnabled: (enabled !== undefined) ? enabled : true ,
       ruleType: type,
       ruleSearch: search,
       ruleReplace: replace,
@@ -433,6 +454,7 @@
 
 
   cancelButton.on("click", stopEdit);
+
 
   // make a few calls
   getCurrentURL(url => {
