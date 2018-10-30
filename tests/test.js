23da00868fc50768abef925260422f345668bcbd
spl <soos.peter.levente@protonmail.com>
2018-10-30 13:52:33 +0100

    placeholder test

--- tests/test.js
+++ tests/test.js
@@ -1,21 +1,9 @@
 prefixLog = (...args) => console.log;
-var Rule = require("../src/background/lib/Rule.js");
 
-var rule = new Rule({
-  ruleIsEnabled: true,
-  ruleType: "string",
-  ruleSearch: "aaa",
-  ruleReplace: "bbb"
-});
+describe("hello world", () => {
 
-describe("test", function () {
-
-  it("works", function () {
+  it("works", () => {
     expect(true).toBe(true);
-  });
-
-  it("has a Rule", function () {
-    expect(typeof rule).toBe('object');
   });
 
 });
